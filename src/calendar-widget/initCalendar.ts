3f8def29660ca775105cb2fb37fe0b439656b984
林东吴 <lindongwu@bytedance.com>
2022-12-02 01:01:26 +0800

    refactor: move handlers to separate file

--- src/calendar-widget/initCalendar.ts
+++ src/calendar-widget/initCalendar.ts
@@ -1,4 +1,3 @@
-import type { ModalWidget } from 'tiddlywiki';
 import { Calendar, EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -6,9 +5,7 @@ import scrollGridPlugin from '@fullcalendar/scrollgrid';
 import adaptivePlugin from '@fullcalendar/adaptive';
 import interactionPlugin from '@fullcalendar/interaction';
 import listPlugin from '@fullcalendar/list';
-import { toTWUTCString } from '../utils';
-
-const TWModal = (require('$:/core/modules/utils/dom/modal.js') as { Modal: ModalWidget }).Modal;
+import { getHandlers } from './handlers';
 
 const isSmallScreen = window.innerWidth <= 600;
 
@@ -52,46 +49,7 @@ export function initCalendar(containerElement: HTMLDivElement, events: EventInpu
       : false,
     schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
     // event handlers
-    eventClick: (info) => {
-      if (info.jsEvent.getModifierState('Control') || info.jsEvent.getModifierState('Meta')) {
-        // DEBUG: console
-        console.log(`info.event.id`, info.event.id);
-      } else {
-        // DEBUG: console
-        console.log(`info.event`, info.event);
-      }
-    },
-    dateClick(info) {
-      console.log('dateClick', info);
-    },
-    /**
-     * Trigger when user select by mouse or long-press and drag on the grid, no matter it is empty or has event
-     * @url https://fullcalendar.io/docs/select-callback
-     */
-    select(info) {
-      // @ts-expect-error Property 'type' does not exist on type 'ViewApi'.ts(2339)
-      if (info.view.type === 'dayGridMonth') {
-        info.start = new Date(info.startStr);
-        info.end = new Date(info.endStr);
-        // info.end.setDate(info.start.getDate() - 1);
-      }
-      const startDate = toTWUTCString(info.start);
-      const endDate = toTWUTCString(info.end);
-      $tw.wiki.addTiddler({
-        title: '$:/state/Calendar/PageLayout/create-tiddler',
-        startDate,
-        endDate,
-        'draft.title': info.startStr,
-      });
-      new TWModal($tw.wiki).display('$:/plugins/linonetwo/tw-calendar/calendar-widget/tiddlywiki-ui/CreateNewTiddlerPopup', {
-        variables: { maskClosable: 'true' },
-      });
-      const titleInputElement = document.querySelector<HTMLInputElement>('.tw-calendar-layout-create-new-tiddler-popup > .tc-titlebar.tc-edit-texteditor');
-      if (titleInputElement !== null) {
-        // fix title not auto focus in modal
-        titleInputElement.focus();
-      }
-    },
+    ...getHandlers(),
   });
   return calendar;
 }
