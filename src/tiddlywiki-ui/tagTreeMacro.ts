fe864e5a3f1a821508036ac1c079398838dd72f9
lin onetwo <linonetwo012@gmail.com>
2022-12-07 16:14:45 +0800

    feat: render frequently used tag tree

---
+++ src/tiddlywiki-ui/tagTreeMacro.ts
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable unicorn/prevent-abbreviations */
+/*
+Display passed-in tiddler names in a tree like <<toc>> respect to their tag relationship
+*/
+
+exports.name = 'input-only-tag-tree';
+
+exports.params = [{ name: 'tags' }];
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const contrastcolour: (colour: string, fallbackTarget: string, colourA: string, colourB: string) => string =
+  require('$:/core/modules/macros/contrastcolour.js').run;
+
+const textMacros = `
+\\define tag-styles()
+background-color:$(backgroundColor)$;
+fill:$(foregroundColor)$;
+color:$(foregroundColor)$;
+\\end
+
+\\define tag-body-inner(colour,fallbackTarget,colourA,colourB,icon)
+\\whitespace trim
+<$vars foregroundColor=<<contrastcolour target:"""$colour$""" fallbackTarget:"""$fallbackTarget$""" colourA:"""$colourA$""" colourB:"""$colourB$""">> backgroundColor="""$colour$""">
+<$button class="tc-tag-label" style=<<tag-styles>>>
+<$transclude tiddler="""$icon$"""/><$view field="title" format="text" />
+</$button>
+</$vars>
+\\end
+`;
+
+exports.run = function (tagsString = '') {
+  const tags = tagsString.split(' ');
+  // remove all tags that has direct parent in the array, so we won't create duplicate trees
+  const tagsWithoutDuplicate = tags.filter((tag) => {
+    const tiddler = $tw.wiki.getTiddler(tag);
+    if (tiddler === undefined) return false;
+    if ((tiddler.fields.tags ?? []).some((tagOfTag) => tags.includes(tagOfTag))) return false;
+    return true;
+  });
+  if (tagsWithoutDuplicate.length === 0) return '';
+  return `${textMacros}<ul>
+    ${tagsWithoutDuplicate
+      .map((tag) => buildTocWithOfferedTiddlers(tag, tags))
+      .map((item) => `<li>${item}</li>`)
+      .join('')}
+    </ul>`;
+};
+
+function buildTocWithOfferedTiddlers(rootTiddler: string, offeredTiddlers: string[]): string {
+  const tiddlersTaggingTheRoot = $tw.wiki.filterTiddlers(`[tag[${rootTiddler}]]`);
+  const paletteName = $tw.wiki.getTiddlerText('$:/palette') ?? '$:/palettes/Vanilla';
+  const rootButton = `
+  <$let
+    currentTiddler="${rootTiddler}"
+    icon={{{ [[${rootTiddler}]] :cascade[all[shadows+tiddlers]tag[$:/tags/TiddlerIconFilter]!is[draft]get[text]] }}}
+    fallbackTarget={{${paletteName}##tag-background}}
+    colourA={{${paletteName}##foreground}}
+    colourB={{${paletteName}##background}}
+    colour={{{ [[${rootTiddler}]] :cascade[all[shadows+tiddlers]tag[$:/tags/TiddlerColourFilter]!is[draft]get[text]] }}}
+  >
+    <$macrocall $name="tag-body-inner" colour=<<colour>> fallbackTarget=<<fallbackTarget>> colourA=<<colourA>> colourB=<<colourB> icon=<<icon>>/>
+  </$let>`;
+  if (tiddlersTaggingTheRoot.length === 0) return rootButton;
+  return `${rootButton}
+  <ul>
+	${tiddlersTaggingTheRoot.map((tag) => `<li>${buildTocWithOfferedTiddlers(tag, offeredTiddlers)}</li>`).join('')}
+	</ul>`;
+}
