f12fe63f8fdb5cf0912cc6b1785f90c452d33b8e
linonetwo <linonetwo012@gmail.com>
2024-02-06 23:30:35 +0800

    docs: move comment and commentout visibleRange which is not using

--- src/tiddlywiki-calendar/calendar-widget/customView.ts
+++ src/tiddlywiki-calendar/calendar-widget/customView.ts
@@ -24,8 +24,9 @@ export function getCustomViews(locale: LocaleSingularArg): CalendarOptions['view
     timeGridThreeDay: {
       type: 'timeGrid',
       buttonText: locale === 'zh-cn' ? '3日' : (getIsSmallScreen() ? '3d' : '3 day'),
-      duration: { days: 3 }, // comment out this after https://github.com/fullcalendar/fullcalendar/issues/7129 solved. the duration option will override the visibleRange option
-      visibleRange: threeDayWith1Previous1NextVisibleRange,
+      duration: { days: 3 },
+      // comment out this after https://github.com/fullcalendar/fullcalendar/issues/7129 solved. the duration option will override the visibleRange option
+      // visibleRange: threeDayWith1Previous1NextVisibleRange,
     },
     timeGridDay: {
       type: 'timeGrid',
